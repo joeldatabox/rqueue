c2efb3f5fc7242bf2540ac56d8660fb7c2d567bb
Sonu Kumar <sonunitw12@gmail.com>
2020-05-03 13:56:11 +0530

    Import fixes

--- rqueue-spring-example/src/main/java/rqueue/spring/example/MvcConfig.java
+++ rqueue-spring-example/src/main/java/rqueue/spring/example/MvcConfig.java
@@ -16,12 +16,12 @@
 
 package rqueue.spring.example;
 
+import com.github.sonus21.test.LoggerInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import com.github.sonus21.rqueue.test.LoggerInterceptor;
 
 @EnableWebMvc
 @Configuration
